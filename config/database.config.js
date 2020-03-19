d50f9a6e64d5b42cdae05dc12905f269b6cf4409
Cocoalabs <rahul.cocoalabs.com>
2020-03-19 18:06:43 +0530

    db change

--- config/database.config.js
+++ config/database.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   development: {
-    // url: 'mongodb://localhost:27017/edunet-db'       
+      // url: 'mongodb://localhost:27017/filmy'
     url: 'mongodb://localhost:27017/filmy'
   },
   qa: {
-    url: 'mongodb+srv://developer:T4zs9mYUTrkWj37@edu2-os2mq.mongodb.net/Edunet?retryWrites=true&w=majority'
+      url: 'mongodb://localhost:27017/filmy',
+
+    // url: 'mongodb+srv://developer:T4zs9mYUTrkWj37@edu2-os2mq.mongodb.net/Edunet?retryWrites=true&w=majority'
   }
 
 }
