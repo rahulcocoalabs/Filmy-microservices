eea1a54291c9644c1a07a943f09f9a47e287f176
shefin <shefin@cocoalabs.in>
2020-03-27 18:40:24 +0530

    ecosystem changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,5 +14,19 @@ module.exports = {
         port : 4001
       }
     },
+    {
+      name: 'Feeds Filmy Microservices',
+      script: 'accounts.service.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      //cron_restart
+      env: {
+        NODE_ENV: 'development',
+        port : 4002
+      }
+    },
     ]
   };
